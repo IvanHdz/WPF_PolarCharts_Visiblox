053ec883864ebb4f02276664e0c5db5603a0fdcd
Jesús Iván Hernández Ramírez <Jesus_Hdez.Ramirez@outlook.com>
2020-08-13 10:42:12 -0500

    Commit

---
+++ Polar.Charts/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace Polar.Charts
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const double k = 7;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = Enumerable.Range(0, 1000)
+                .Select(i => 2 * Math.PI * i / 1000.0)
+                .Select(t => new
+                {
+                    Theta = t,
+                    R = Math.Cos(k * t),
+
+                    X = Math.Cos(k * t) * Math.Sin(t),
+                    Y = Math.Cos(k * t) * Math.Cos(t)
+                });
+        }
+    }
+}
